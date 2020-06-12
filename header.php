d47bd9454241cb6586a0ca74cb98e68ed1a283e8
Ben Allison <spamtime78@gmail.com>
2020-06-12 12:23:02 +1000

    Updated Header to have a dashboard button

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
         <nav class="navbar navbar-expand navbar-dark bg-dark sticky-top">
             <a href="index.php" class="navbar-brand">Teacher Class Manager</a>
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="header.php" class="nav-link">link 1</a></li>
+                <li class="nav-item"><a href="dashboard.php" class="nav-link">Dashboard</a></li>
                 <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
                 <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
             </ul>
