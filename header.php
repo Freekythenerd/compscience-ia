b10b03040594cece39cba6bcda8d3fd383f443ab
Ben Allison <spamtime78@gmail.com>
2020-06-14 11:53:11 +1000

    Added check to see if user is logged in which adds more buttons

--- header.php
+++ header.php
@@ -5,6 +5,13 @@
                 <li class="nav-item"><a href="dashboard.php" class="nav-link">Dashboard</a></li>
                 <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
                 <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+                <?php
+    if (isset($_SESSION["id"])) // check if user is logged in
+        echo '<li class="nav-item"><a href="studentmaintenance.php" class="nav-link">Student Maintenance</a></li>';
+        echo '<li class="nav-item"><a href="classmaintenance.php" class="nav-link">Class Maintenance</a></li>';
+        echo '<li class="nav-item"><a href="logout.php" class="nav-link">Logout</a></li>';
+?>
+                
             </ul>
         </nav>
     </header>
